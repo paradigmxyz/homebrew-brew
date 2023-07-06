6eaad78e009de1efab73f173083071646413f024
Oliver Nordbjerg <hi@notbjerg.me>
2023-07-06 02:45:47 +0200

    feat: reth formula

---
+++ Formula/reth.rb
@@ -0,0 +1,27 @@
+class Reth < Formula
+  desc "A fast implementation of the Ethereum protocol in Rust"
+  homepage "https://github.com/paradigmxyz/reth"
+  url "https://github.com/paradigmxyz/reth/archive/v0.1.0-alpha.1.tar.gz"
+  sha256 "768cc9a247ca56b441ea994c7b088a03c1e36d40ecf1001dda6d600159e84472"
+  license any_of: ["Apache-2.0", "MIT"]
+  head "https://github.com/paradigmxyz/reth.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "llvm" => :build
+  depends_on "pkg-config" => :build
+  depends_on "rust" => :build
+
+  def install
+    cd "bin/reth" do
+      system "cargo", "install", "--bin", "reth", "--profile", "maxperf", "--features", "jemalloc", *std_cargo_args
+    end
+  end
+
+  test do
+    system "#{bin}/reth", "--help"
+  end
+end
