04d9c11f0b4aea87623bfc9b6aa2f2947a2e7f8d
Oliver Nordbjerg <hi@notbjerg.me>
2023-07-10 18:35:42 +0200

    release: reth v0.1.0-alpha.3

--- Formula/reth.rb
+++ Formula/reth.rb
@@ -1,7 +1,9 @@
 class Reth < Formula
   desc "Fast implementation of the Ethereum protocol in Rust"
   homepage "https://github.com/paradigmxyz/reth"
-  url "https://github.com/paradigmxyz/reth/archive/v0.1.0-alpha.2.tar.gz"
+  url "https://github.com/paradigmxyz/reth.git",
+    tag: "v0.1.0-alpha.3",
+    revision: "31af4d55bc5ad5fe976ddf4aa50ce441699e5f9c"
   sha256 "7d8ca049db6042dcab4338386d9334556da188ca81408fd86c2d5ffc93b07617"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/paradigmxyz/reth.git", branch: "main"
