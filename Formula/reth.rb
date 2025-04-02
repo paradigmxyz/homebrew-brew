c3382c98bd4e98acabceb137f8d0c9230f6061c3
Matthias Seitz <19890894+mattsse@users.noreply.github.com>
2025-04-02 19:09:20 +0000

    reth: update 1.3.5 bottle.

--- Formula/reth.rb
+++ Formula/reth.rb
@@ -13,9 +13,9 @@ class Reth < Formula
   end
 
   bottle do
-    root_url "https://github.com/paradigmxyz/homebrew-brew/releases/download/reth-1.3.4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "358a9093a45bd1d997448014883031a8bd9a19f429228708eb8188ae6f0de9f1"
-    sha256 cellar: :any_skip_relocation, ventura:      "7216b53b5565a3f4020afeed4df76364085fe90c1e64cddb986c20613f31d45a"
+    root_url "https://github.com/paradigmxyz/homebrew-brew/releases/download/reth-1.3.5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "19449d42f3990c5def91ac03c6117bc4a608f06ed7b67b759a995788e147a03b"
+    sha256 cellar: :any_skip_relocation, ventura:      "ca07f2bfa543d7d058c265ac2e0e9628066b656b07f6f5ddc7eca2986d1a0ee7"
   end
 
   depends_on "pkg-config" => :build
