2a7fcd9911e755cd723fd9af96ee99d17a8d7e4c
Matthias Seitz <matthias.seitz@outlook.de>
2025-05-27 20:50:08 +0200

    chore: release 1.4.3

--- Formula/reth.rb
+++ Formula/reth.rb
@@ -2,8 +2,8 @@ class Reth < Formula
   desc "Fast implementation of the Ethereum protocol in Rust"
   homepage "https://github.com/paradigmxyz/reth"
   url "https://github.com/paradigmxyz/reth.git",
-    tag:      "v1.4.1",
-    revision: "e6ce41ebba0d8752cef9ed885aae057e09226d05"
+    tag:      "v1.4.3",
+    revision: "fe3653ffe602d4e85ad213e8bd9f06e7b710c0c5"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/paradigmxyz/reth.git", branch: "main"
 
